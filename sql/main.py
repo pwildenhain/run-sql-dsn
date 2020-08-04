a24a7bdc48c5a98e90cad76e0fd624cf5678f2af
pwildenhain <pwildenhain@gmail.com>
2020-08-04 19:00:39 -0400

    :sparkles: Add minimally working CLI
    With good docs and doing things the "typer" way

--- sql/main.py
+++ sql/main.py
@@ -1,18 +1,38 @@
 import pyodbc
 from tabulate import tabulate
+import typer
 
-# Remember to start dev server before running script
-# password is postgres
-conn = pyodbc.connect("dsn=PostgreSQL30")
+app = typer.Typer()
 
-cursor = conn.cursor()
 
-result = cursor.execute("select * from test_db.public.example_table").fetchall()
+@app.command()
+def run(
+    sql: str = typer.Argument(..., help="SQL string to execute"),
+    dsn: str = typer.Argument(
+        "",
+        help="ODBC dsn to connect to through pyodbc.connect()",
+        envvar="RUN_SQL_DSN",
+        show_default=False,
+    ),
+    column_limit: int = typer.Option(10, help="Number of columns to display in output"),
+    row_limit: int = typer.Option(10, help="Number of rows to display in output"),
+    display_format: str = typer.Option(
+        "grid",
+        help="Display style for output. See the tabulate package for more details",
+    ),
+):
+    """Run a SQL query using and ODBC data source name (dsn)"""
+    conn = pyodbc.connect(f"dsn={dsn}")
+    cursor = conn.cursor()
+    cursor.arraysize = row_limit
+    result = cursor.execute(sql).fetchmany()
+    conn.close()
 
-col_names = [column[0] for column in cursor.description]
+    col_names = [typer.style(column[0], bold=True) for column in cursor.description]
+    tablular_data = tabulate(result, headers=col_names, tablefmt=display_format)
 
-col_names
+    typer.echo(tablular_data)
 
-print(tabulate(result, headers=col_names, tablefmt="grid"))
 
-# think through what functions should look like, then think through basic tests
+if __name__ == "__main__":
+    app()
